6fc8bf1332fe905792643e28dc0394cc593b081e
serhiipianykh <serhiipianykh@gmail.com>
2017-02-16 19:20:08 -0500

    initial commit with ui set up

---
+++ WeatherApp/WeatherVC.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherVC.swift
+//  WeatherApp
+//
+//  Created by Serhii Pianykh on 2017-02-05.
+//  Copyright © 2017 Serhii Pianykh. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currTemperatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currWeatherIconImage: UIImageView!
+    @IBOutlet weak var currWeatherLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+
